caa2e1bafe2a1b9be76027016dc4b78df9318cdc
ngothanhcong2882 <80748950+ngothanhcong2882@users.noreply.github.com>
2021-08-18 13:30:42 +0700

    APPASM

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req,res)=>{
-    res.end("hello app new")
+    res.end("I love")
 })
 
 const PORT = process.env.PORT || 5000;
